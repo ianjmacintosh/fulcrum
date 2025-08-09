ba5e947c62aee5d924c1108e633e07ca7242a290
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-08-09 16:39:48 +0000

    Support starting a server

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
     },
     plugins: [
         tsConfigPaths(),
-        tanstackStart({ customViteReactPlugin: true }),
+        tanstackStart({ 
+            target: 'node-server',
+            customViteReactPlugin: true 
+        }),
         viteReact(),
     ],
 })
