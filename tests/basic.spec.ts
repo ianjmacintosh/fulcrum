38808b88f3c5883c4a013e7ca5cd1f84eed8726f
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-08-15 14:30:32 +0000

    Include basic tests

---
+++ tests/basic.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://fulcrum.ianjmacintosh.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Fulcrum/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://fulcrum.ianjmacintosh.com/');
+
+  // Click the log in link.
+  await page.getByRole('link', { name: 'Login' }).click();
+
+  // Expects page to have a heading with the name of Sign In.
+  await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible();
+});
