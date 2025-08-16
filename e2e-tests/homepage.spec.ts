b12d1fbf5382c1786c93d694cbff33399a04439a
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-08-16 15:58:39 +0000

    Add new test ONLY to check if the site loads

---
+++ e2e-tests/homepage.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+test('Homepage loads', async ({ page }) => {
+    await page.goto('/');
+
+    await expect(page.getByRole('heading', { name: 'Welcome to Fulcrum' })).toBeVisible();
+});
